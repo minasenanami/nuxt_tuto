5ce473f7d0ff285a1964d31c527ba291d51c6a79
僚 <>
2021-10-04 12:11:04 +0900

    nuxt-linkを使用したページ遷移

--- pages/articles/_id/edit.vue
+++ pages/articles/_id/edit.vue
@@ -1,12 +1,3 @@
 <template>
   <div>編集ページ</div>
 </template>
-
-<script>
-export default {
-  mounted() {
-    const foo = '東山奈央'
-    console.log(foo)
-  },
-}
-</script>
--- pages/articles/_id/index.vue
+++ pages/articles/_id/index.vue
@@ -1,12 +1,11 @@
 <template>
-  <div>詳細ページ</div>
+  <div>
+    <div>詳細ページ</div>
+    <div>
+      <nuxt-link to="/articles/10/edit">Nuxt</nuxt-link>
+    </div>
+    <div>
+      <a href="/articles/10/edit">aタグ</a>
+    </div>
+  </div>
 </template>
-
-<script>
-export default {
-  mounted() {
-    const foo = '東山奈央'
-    console.log(foo)
-  },
-}
-</script>
